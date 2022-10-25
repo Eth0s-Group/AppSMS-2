20b48bf98cd3df2970a1957ea26725c5f27044a9
SpaceSaver <spacesaverundercoverdev@gmail.com>
2022-10-24 22:51:23 -0400

    Define initial goals in comment

---
+++ module.js
@@ -0,0 +1,8 @@
+class Module {
+    constructor() {
+
+    }
+    trigger() {
+        
+    }
+}
--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/* Summary of build:
+ * 1. Create standardized class for interacting with users
+        (Improves scalability)
+        - Replying
+        - Waiting for a reply
+        - On message
+ * 2. Create standard module class
+        - Common trigger function
+        - Common "on reply" function for handling followups
+        - Common method for signaling that a module is waiting for a response
+ */
 const TN4NJS = require("./TN4NJS/TNAPI");
 const TNcli = new TN4NJS();
 const { username, sid, csrf, owmtoken } = require("./credentials.json");
@@ -11,8 +22,8 @@ async function main() {
 
 /**
  * Handle incoming messages.
- * @param {TN4NJS.Message} msg 
+ * @param {TN4NJS.Message} msg Incoming message.
  */
 async function handleCMD(msg) {
-    msg
+    
 }
