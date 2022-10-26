1746f8b726e1b98bf498013b9103bf67727fff73
SpaceSaver <spacesaverundercoverdev@gmail.com>
2022-10-25 22:30:54 -0400

    Start stuff

---
+++ BaseCommModule.js
@@ -0,0 +1,18 @@
+const { EventEmitter } = require("events");
+
+class CommunicationsModule extends EventEmitter{
+    constructor() {
+        
+    }
+
+    send(identifier, message){
+        
+    }
+    send_image(identifier){
+
+    }
+    
+}
+class CommunicationsModuleMessage {
+
+}
